9afa52993ecb517919e137bbcf7fa175215c34ee
Nata Nael <natanael280198@gmail.com>
2023-09-21 21:34:43 +0700

    feat (script.js): cvv and card exp input function

--- interactive-card-details-form-main/script.js
+++ interactive-card-details-form-main/script.js
@@ -2,6 +2,12 @@ const cardHolderNameInput = document.getElementById("cardHolderNameInput");
 const cardHolderName = document.getElementById("cardHolderName");
 const cardNumberInput = document.getElementById("cardNumberInput");
 const cardNumber = document.getElementById("cardNumber");
+const monthExpInput = document.getElementById("monthExpInput");
+const yearExpInput = document.getElementById("yearExpInput");
+const expMonth = document.getElementById("expMonth");
+const expYear = document.getElementById("expYear");
+const cardCvv = document.getElementById("cardCvv");
+const cardCvvInput = document.getElementById("cardCvvInput");
 
 cardHolderNameInput.addEventListener("input", (event) => {
   const inputValue = event.target.value;
@@ -27,5 +33,41 @@ cardNumberInput.addEventListener("input", (event) => {
 
   if (inputValue.length === 0) {
     cardNumber.innerHTML = "0000 0000 0000 0000";
+  }
+});
+
+monthExpInput.addEventListener("input", (event) => {
+  const inputValue = event.target.value;
+
+  if (inputValue.length <= 2) {
+    expMonth.innerHTML = inputValue;
+  }
+
+  if (inputValue.length === 0) {
+    expMonth.innerHTML = "00";
+  }
+});
+
+yearExpInput.addEventListener("input", (event) => {
+  const inputValue = event.target.value;
+
+  if (inputValue.length <= 2) {
+    expYear.innerHTML = inputValue;
+  }
+
+  if (inputValue.length === 0) {
+    expYear.innerHTML = "00";
+  }
+});
+
+cardCvvInput.addEventListener("input", (event) => {
+  const inputValue = event.target.value;
+
+  if (inputValue.length <= 3) {
+    cardCvv.innerHTML = inputValue;
+  }
+
+  if (inputValue.length === 0) {
+    cardCvv.innerHTML = "000";
   }
 });
